780bb09ee4718df7ad6c742557fb6dcc4d78237a
Vasek Hervert <vasekhervert@gmail.com>
2020-01-26 17:11:53 +0100

    added support for adding classes to columns

--- client/src/components/Grid.js
+++ client/src/components/Grid.js
@@ -1,9 +1,13 @@
 import React from "react";
 
 export function Row({ classes, children }) {
-  return <div className={`row ${classes}`}>{children}</div>;
+  return <div className={`row ${classes ? classes : ""}`}>{children}</div>;
 }
 
-export function Column({ children, large }) {
-  return <div className={`columns large-${large}`}>{children}</div>;
+export function Column({ children, large, classes }) {
+  return (
+    <div className={`columns large-${large} ${classes ? classes : ""}`}>
+      {children}
+    </div>
+  );
 }
